d1d9099c2b615beeab18a6aeea12d746a79b8fb7
Abdulshakourr <shakirteck4@gmail.com>
2023-06-16 13:58:30 -0700

    fixed

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,20 +11,24 @@ int number_left; /*Decarling statement*/
 int number_right;
 
 /*for 0-9 with ASCII*/
+
 for (number_left = 48; number_left <= 57; number_left++)
 {
-for (number_right = 48; number_right <= 57; number_right++)
+for (number_right = number_left + 1 ; number_right <= 57; number_right++)
 {
-putchar(number_left); /*print  number*/
+
+putchar(number_left);
 putchar (number_right);
 
-/*break program if the condition is true (if is 99)*/
-if ((number_left == 57) && (number_right == 57))
+/*break program if the condition is true (if is 99=+)*/
+if ((number_left == 56) && (number_right == 57))
 {
-break; /*terminate the program*/
+break;/*terminate the program*/
 }
+
 putchar(','); /*print (,)*/
-putchar (' '); /*print a space*/
+putchar (' '); /*prirint a space*/
+
 }
 
 }
